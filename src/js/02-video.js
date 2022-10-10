b7f66b660a3405b56614a4de3fbb340fdd283e93
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-10-10 21:57:47 +0200

    Zadanie 2 wykonane

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+var _ = require('lodash');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_TIME = "videoplayer-current-time";
 
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlay = function(data) {
+    localStorage.setItem(LOCALSTORAGE_TIME, data.seconds);
+    console.log("ok");
+};
+
+player.on('timeupdate',  _.throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_TIME)); 
