5a355865e4d866bbf28ed7a3d1adb3a55e9171d9
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-10-15 16:58:57 +0200

    1 i 2 zadanie z pracy domowej wykonane, 3 zaczęte, punkt 1 i 2 trzeciego zadania skończony, 3 w trakcie

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+var _ = require('lodash');
 
+const wholeForm = document.querySelector(".feedback-form");
+const emailInput = document.querySelector("input[name='email']");
+const messageInput = document.querySelector("textarea[name='message']");
+const submitButton = document.querySelector("button");
+
+// let LOCALSTORAGE_INPUTS = 'feedback-form-state';
+
+var existing = localStorage.getItem('feedback-form-state');
+
+existing = existing ? JSON.parse(existing) : {};
+
+emailInput.value = existing.email;
+messageInput.value = existing.message;
+
+wholeForm.addEventListener("input", () => {
+    existing['email'] = emailInput.value;
+    existing['message'] = messageInput.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(existing));
+});
+
+submitButton.addEventListener("click", () => {
+    const output = localStorage.getItem('feedback-form-state');
+    // localStorage.clear();
+    // wholeForm.reset();
+    localStorage.clear();
+    return output;
+});
